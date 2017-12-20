22240fdd361ac3371f068c0de1eda35ad99e0c06
Neeraj Poddar <neeraj@aspenmesh.io>
2017-12-20 16:22:16 -0700

    Added documentation for vetter and meshclient packages.

--- pkg/meshclient/client.go
+++ pkg/meshclient/client.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Package meshclient provides utilities for creating a kubernetes client.
 package meshclient
 
 import (
